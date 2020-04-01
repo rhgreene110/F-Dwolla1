1c7059df6273a624854d4f1fea1b9d7c456dd345
Richard Greene <rhgreene110@mac.com>
2020-04-01 12:54:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ var dwolla = require ('dwolla-v2');
 //require ('dotenv').config ();
 require ('dotenv').config ({path: __dirname + '/.env'});
 
-// Show contents of .env file
+// RHG - Show contents of .env file
 console.log ('RHG-ENV DWOLLA_APP_KEY --- ', process.env.DWOLLA_APP_KEY);
 console.log ('RHG-ENV DWOLLA_APP_SECRET --- ', process.env.DWOLLA_APP_SECRET);
 
-var merchant = new dwolla.Client ({
+var merchant1 = new dwolla.Client ({
   key: process.env.DWOLLA_APP_KEY,
   secret: process.env.DWOLLA_APP_SECRET,
   environment: 'sandbox', // defaults to 'production'
 });
 
-console.log ('Merchant ', merchant);
+console.log ('Merchant1 ', merchant1);
 
 var customer1 = new dwolla.Client ({
   key: process.env.DWOLLA_APP_KEY,
